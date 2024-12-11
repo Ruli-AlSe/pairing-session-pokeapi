c551fac788f4803b7544d12eab83e8085e970dd1
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-12-11 02:36:40 -0600

    Implement some styles for shadows and animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'e-md': '3px 3px 9px -3px rgb(0 0 0 / 0.3)',
+      },
+      animation: {
+        fade: 'fade-in 1s ease-in-out',
+      },
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+    },
   },
   plugins: [],
 };
